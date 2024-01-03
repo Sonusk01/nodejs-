c9a4dcad107aef8174c8883577cc72af6140782b
Sonusk01 <118705498+Sonusk01@users.noreply.github.com>
2024-01-03 23:28:17 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = false
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// "NUll"  => 0
+// "Undefined" => NaN
+//  true => 1; false => 0;
+
+let isLoggedIn = "sonu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//  1 => true; 0 => false
+// "" => false
+// "Hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
